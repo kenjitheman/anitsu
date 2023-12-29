d931fc8ee37c9ee89ce0828731fa53b04de9c095
kenjitheman <kenjith3man@gmail.com>
2023-12-29 15:15:45 +0200

    feat: implement main.go

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	jikan "github.com/darenliang/jikan-go"
+	"github.com/kenjitheman/anisync/api"
+)
+
+func main() {
+	filter := parseFlags()
+
+	data, err := api.GetData(filter)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(data)
+}
+
+func parseFlags() jikan.ScheduleFilter {
+	var day string
+
+	flag.StringVar(&day, "day", "monday", "Day of the week")
+	flag.Parse()
+
+	return jikan.ScheduleFilter(day)
+}
